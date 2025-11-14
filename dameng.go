b26abeea29376298ff5448a742fdcf477b91b3ff
yingjieyi <yingjieyi@tencent.com>
2025-11-14 16:08:30 +0800

    fix: 优化连接配置方法，添加默认属性

--- dameng.go
+++ dameng.go
@@ -14,10 +14,54 @@ const DriverName = "dm"
 //   - 如： dm://user:password@host:port?schema=SYSDBA[&propName2=propValue2]…
 //   - 若要指定用户登录后的当前模式，请在 options 中设置 schema，缺省为用户的默认模式，如 SYSDBA
 //   - 参考链接： https://eco.dameng.com/document/dm/zh-cn/pm/go-rogramming-guide.html#11.3%20%E8%BF%9E%E6%8E%A5%E4%B8%B2%E5%B1%9E%E6%80%A7%E8%AF%B4%E6%98%8E
-func BuildUrl(user, password, host string, port int, urlOptions ...map[string]string) string {
+type ConnectionConfig struct {
+	user string
+	password string
+	host string
+	port int
+	props map[string][]string
+}
+
+type withOption func(config *ConnectionConfig)
+
+func WithProp(key string, value string) withOption {
+	return func(config *ConnectionConfig) {
+		config.props[key] = append(config.props[key], value)
+	}
+}
+
+func NewConnectionConfig(user, password, host string, port int, schema string, propsFunc ...withOption) *ConnectionConfig {
+	config := &ConnectionConfig{
+		user: user,
+		password: password,
+		host: host,
+		port: port,
+		props: make(map[string][]string),
+	}
+	for _, p := range propsFunc {
+		p(config)
+	}
+
+	if schema != "" && config.props["schema"] == nil {
+		config.props["schema"] = []string{schema}
+	}
+	if config.props["columnNameCase"] == nil {
+		config.props["columnNameCase"] = []string{"lower"}
+	}
+	if config.props["escapeProcess"] == nil {
+		config.props["escapeProcess"] = []string{"true"}
+	}
+	return config
+}
+
+func (c *ConnectionConfig) BuildUrl() string {
+	return BuildUrl(c.user, c.password, c.host, c.port, c.props)
+}
+
+func BuildUrl(user, password, host string, port int, urlOptions map[string][]string) string {
 	propQuery := url.Values{}
-	for _, option := range urlOptions {
-		for key, value := range option {
+	for key, option := range urlOptions {
+		for _, value := range option {
 			propQuery.Add(key, value)
 		}
 	}
